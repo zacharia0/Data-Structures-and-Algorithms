ced701a44c2039c82e1dfd02bc934560ee80282a
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-16 15:29:53 -0600

    Added the pop and append method

--- linkedlist.py
+++ linkedlist.py
@@ -24,6 +24,34 @@ class LinkList:
             temp = temp.next
         
 
+    def append(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
 
+        else:
+            self.tail.next = new_node 
+            self.tail = new_node 
+        self.length += 1
+        return True 
+    
 
+    def pop(self):
+        if self.length == 0:
+            return None 
 
+        temp = self.head 
+        pre = self.head 
+
+        while temp.next is not None:
+            pre = temp 
+            temp = temp.next 
+
+        self.tail = pre 
+        self.tail.next = None 
+        self.length -= 1 
+        if self.length == 0:
+            self.head = None 
+            self.tail = None 
+        return temp 
