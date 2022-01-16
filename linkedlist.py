58370c7132e27562d398911df324420b9b2828b2
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-15 21:16:22 -0600

    added the Linklist class which creates a node and also prints the value

--- linkedlist.py
+++ linkedlist.py
@@ -1,8 +1,29 @@
+from hashlib import new
+
+from matplotlib.pyplot import get
+
+
 class Node:
-    """The Node class creates a node"""
+    """The Node class creates a node""" 
     def __init__(self,value):
         """Initialize the constructor"""
         self.value = value 
         self.next = None 
 
+class LinkList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1 
+
+    def print_list(self):
+        temp = self.head 
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
         
+
+
+
+
