07c97d6c6029a733a7d3f182053a623753fa653a
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-15 18:22:19 -0600

    Added the Node class that creates a node.

---
+++ linkedlist.py
@@ -0,0 +1,8 @@
+class Node:
+    """The Node class creates a node"""
+    def __init__(self,value):
+        """Initialize the constructor"""
+        self.value = value 
+        self.next = None 
+
+        
