efd190252af9bba9e379486fcf49f621a3b8f5ec
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-25 21:06:47 -0600

    Added the pop() method which removes the last node of the doubly linked list.

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -29,3 +29,19 @@ def append(self, value):
         self.tail = new_node
     self.length += 1 
     return True 
+
+
+def pop(self):
+    if self.length == 0:
+        return 0
+    temp = self.tail 
+    if self.length == 1:
+        self.head = None 
+        self.tail = None 
+    else:
+        self.tail = self.tail.prev 
+        self.tail.next = None 
+        temp.prev = None 
+    self.length -= 1 
+    return temp 
+
