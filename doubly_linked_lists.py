17359b3b8910994e009c803c056ec90ce1b6293d
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-26 14:50:14 -0600

    Added the prepend() method which adds a node at the beginning of the doubly linked list.

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -1,8 +1,60 @@
+# class Node:
+#     def __init__(self,value):
+#         self.value = value 
+#         self.next = None 
+#         self.prev = None 
+
+# class DoublyLinkedList:
+#     def __init__(self, value):
+#         new_node = Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length = 1 
+
+
+# def print_list(self):
+#     temp = self.head 
+#     while temp is not None:
+#         print(temp.value)
+#         temp = temp.next 
+
+# def append(self, value):
+#     new_node = Node(value)
+#     if self.length == 0:
+#         self.head = new_node
+#         self.tail = new_node
+#     else:
+#         self.tail.next = new_node
+#         new_node.prev = self.tail 
+#         self.tail = new_node
+#     self.length += 1 
+#     return True 
+
+
+# def pop(self):
+#     if self.length == 0:
+#         return 0
+#     temp = self.tail 
+#     if self.length == 1:
+#         self.head = None 
+#         self.tail = None 
+#     else:
+#         self.tail = self.tail.prev 
+#         self.tail.next = None 
+#         temp.prev = None 
+#     self.length -= 1 
+#     return temp 
+
+
+
+
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value 
         self.next = None 
         self.prev = None 
+
+
 
 class DoublyLinkedList:
     def __init__(self, value):
@@ -11,37 +63,50 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1 
 
+    
+    def print_list(self):
+        temp = self.head 
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next 
 
-def print_list(self):
-    temp = self.head 
-    while temp is not None:
-        print(temp.value)
-        temp = temp.next 
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail 
+            self.tail = new_node
+        self.length += 1
+        return True 
 
-def append(self, value):
-    new_node = Node(value)
-    if self.length == 0:
-        self.head = new_node
-        self.tail = new_node
-    else:
-        self.tail.next = new_node
-        new_node.prev = self.tail 
-        self.tail = new_node
-    self.length += 1 
-    return True 
+    
+    def pop(self):
+        if self.length == 0:
+            return None 
+        if self.length == 1:
+            self.tail = None 
+            self.head = None 
+        else:
+            temp = self.tail 
+            self.tail = self.tail.prev 
+            self.tail.next = None 
+            temp.prev = None 
+        self.length -= 1
+        return temp  
 
 
-def pop(self):
-    if self.length == 0:
-        return 0
-    temp = self.tail 
-    if self.length == 1:
-        self.head = None 
-        self.tail = None 
-    else:
-        self.tail = self.tail.prev 
-        self.tail.next = None 
-        temp.prev = None 
-    self.length -= 1 
-    return temp 
-
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head 
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1 
+        return True 
