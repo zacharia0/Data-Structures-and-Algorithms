310db37a55ebc5fc52afe72a9dfaf238cbc1e486
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-25 21:01:45 -0600

    Added the append() method which addes a new node.

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -17,3 +17,15 @@ def print_list(self):
     while temp is not None:
         print(temp.value)
         temp = temp.next 
+
+def append(self, value):
+    new_node = Node(value)
+    if self.length == 0:
+        self.head = new_node
+        self.tail = new_node
+    else:
+        self.tail.next = new_node
+        new_node.prev = self.tail 
+        self.tail = new_node
+    self.length += 1 
+    return True 
