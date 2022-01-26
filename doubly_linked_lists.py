97f5e8994eb61b76d0ddb8a5ad447f5da60bad38
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-26 15:25:04 -0600

    Added the get() method which gets a node in a certain index.

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -1,53 +1,3 @@
-# class Node:
-#     def __init__(self,value):
-#         self.value = value 
-#         self.next = None 
-#         self.prev = None 
-
-# class DoublyLinkedList:
-#     def __init__(self, value):
-#         new_node = Node(value)
-#         self.head = new_node
-#         self.tail = new_node
-#         self.length = 1 
-
-
-# def print_list(self):
-#     temp = self.head 
-#     while temp is not None:
-#         print(temp.value)
-#         temp = temp.next 
-
-# def append(self, value):
-#     new_node = Node(value)
-#     if self.length == 0:
-#         self.head = new_node
-#         self.tail = new_node
-#     else:
-#         self.tail.next = new_node
-#         new_node.prev = self.tail 
-#         self.tail = new_node
-#     self.length += 1 
-#     return True 
-
-
-# def pop(self):
-#     if self.length == 0:
-#         return 0
-#     temp = self.tail 
-#     if self.length == 1:
-#         self.head = None 
-#         self.tail = None 
-#     else:
-#         self.tail = self.tail.prev 
-#         self.tail.next = None 
-#         temp.prev = None 
-#     self.length -= 1 
-#     return temp 
-
-
-
-
 class Node:
     def __init__(self, value):
         self.value = value 
@@ -126,13 +76,20 @@ class DoublyLinkedList:
             temp.next = None 
         self.length -= 1 
         return temp 
+
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None 
+        temp = self.head 
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next 
+        else:
+            temp = self.tail 
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp 
+
+
          
-
-
-my = DoublyLinkedList(0)
-my.append(1)
-my.append(2)
-my.prepop()
-my.prepop()
-my.prepop()
-my.print_list()
