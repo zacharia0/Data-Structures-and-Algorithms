2c2abf9240f4e3d48ccb48f9656df72ce1e37a2b
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-25 20:59:23 -0600

    added the print_list() method

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -1,40 +1,19 @@
-
-
-
 class Node:
     def __init__(self,value):
         self.value = value 
         self.next = None 
-        self.previous = None 
+        self.prev = None 
 
 class DoublyLinkedList:
-    def __init__(self,value):
+    def __init__(self, value):
         new_node = Node(value)
-        self.head = new_node 
+        self.head = new_node
         self.tail = new_node
-        self.length = 1
+        self.length = 1 
 
 
-    def print_list(self):
-        temp = self.head 
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next 
-
-    def push(self,value):
-        new_node = Node(value)
-        if self.length == 0:
-            self.head = new_node 
-            self.tail = new_node 
-        else:
-            self.tail.next = new_node 
-            new_node.previous = self.tail 
-            self.tail = new_node 
-        self.length += 1 
-        return True 
-
-
-
-my = DoublyLinkedList(0)
-my.push(1)
-my.print_list()
+def print_list(self):
+    temp = self.head 
+    while temp is not None:
+        print(temp.value)
+        temp = temp.next 
