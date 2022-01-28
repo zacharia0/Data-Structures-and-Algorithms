468866f0f97e264088fb65e33d75362259fd9eb3
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-28 16:06:27 -0600

    Added the insert() method which inserts a new node into the doubly linked list.

--- doubly_linked_lists.py
+++ doubly_linked_lists.py
@@ -99,4 +99,22 @@ class DoublyLinkedList:
             temp.value = value 
             return True 
         return False 
-    
+
+
+    def insert(self,index, value):
+        if index < 0 or index >= self.length:
+            return False 
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next 
+        new_node.next = after
+        new_node.prev = before 
+        before.next = new_node
+        after.prev = new_node
+         
+
+
