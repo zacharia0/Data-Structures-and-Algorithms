91f4d276eb3ac7640c62a8e1da7d59d07c9fd5e8
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-30 18:56:02 -0600

    Added the enqueue() method which adds a node at the end of the queue.

--- queue.py
+++ queue.py
@@ -13,4 +13,22 @@ class Queue:
         self.length = 1 
 
 
+    def print_queue(self):
+        temp = self.first 
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next 
+
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1    
+
+
+
 
