0e0b427c45634a89c05627a459a1c054194646d1
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-01-30 18:21:36 -0600

    Added the Node and Stack classes.

---
+++ stacks.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, value):
+        self.value = value 
+        self.next = None 
+
+
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+ 
