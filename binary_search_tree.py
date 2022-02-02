d8fa2283418c6cc7c33e663ec7d96b49536f6ea2
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-01 21:36:34 -0600

    Added the maximum_value_right() method which searches for the largest value in the tree.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -11,46 +11,6 @@ class BinarySearchTree:
         self.root = None 
 
 
-#     def insert(self,value):
-#         new_node = Node(value)
-#         if self.root == None:
-#             self.root = new_node
-#             return True 
-#         temp = self.root
-#         while (True):
-#             if new_node.value == temp.value:
-#                 return False
-#             if new_node.value < temp.value:
-#                 if temp.left is None:
-#                     temp.left = new_node
-#                     return True 
-#                 temp = temp.left 
-#             else:
-#                 if temp.right is None:
-#                     temp.right = new_node
-#                     return True 
-#                 temp = temp.right 
-
-#     def contains(self,value):
-#         if self.root == None:
-#             return False  
-#         temp = self.root 
-#         while temp is not None:
-#             if value < temp.value:
-#                 temp = temp.left 
-#             elif value > temp.value:
-#                 temp = temp.right
-#             else:
-#                 return True 
-#             return False  
-            
-# my = BinarySearchTree()
-# my.insert(4)
-# my.insert(5)
-# print(my.contains(1))
-
-
-
     def insert(self, value):
         new_node = Node(value)
         if self.root == None:
@@ -84,11 +44,15 @@ class BinarySearchTree:
         return False  
 
 
-    def minimum_value(self, current_node):
+    def minimum_value_left(self, current_node):
         while current_node.left is not None:
-            current = current_node.left 
+            current_node = current_node.left 
         return current_node
 
+    def maximum_value_right(self, current_node):
+        while current_node.right is not None:
+            current_node =  current_node.right
+        return current_node
 
 
 
