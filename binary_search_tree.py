46ad9c7ccf66a6d4effab566f42f1be4ab5c8e37
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-01 20:22:27 -0600

    Added the insert() method which adds a new node to the right.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -10,5 +10,37 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None 
 
+    def print_binary_search_tree_right(self):
+        temp = self.root.right
+        while temp is not None:
+            print(temp.value)
+            temp = temp.right 
+    
+    def print_binary_search_tree_left(self):
+        temp = self.root.left
+        while temp.left is not None:
+            print(temp.value)
+            # temp = temp.left 
+
+    def insert(self,value):
+        new_node = Node(value)
+        if self.root == None:
+            self.root = new_node
+            return True 
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True 
+                temp = temp.left 
+            else:
+                if new_node.value > temp.value:
+                    if temp.right is None:
+                        temp.right = new_node
+                        return True 
+                    temp = temp.right 
 
 
