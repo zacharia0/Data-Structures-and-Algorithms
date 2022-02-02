e97650886e985f9842f5b044f6f40f9de405a8fd
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-01 20:48:37 -0600

    Added the contains() method which checks if a value is in the tree.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -11,24 +11,76 @@ class BinarySearchTree:
         self.root = None 
 
 
-    def insert(self,value):
+#     def insert(self,value):
+#         new_node = Node(value)
+#         if self.root == None:
+#             self.root = new_node
+#             return True 
+#         temp = self.root
+#         while (True):
+#             if new_node.value == temp.value:
+#                 return False
+#             if new_node.value < temp.value:
+#                 if temp.left is None:
+#                     temp.left = new_node
+#                     return True 
+#                 temp = temp.left 
+#             else:
+#                 if temp.right is None:
+#                     temp.right = new_node
+#                     return True 
+#                 temp = temp.right 
+
+#     def contains(self,value):
+#         if self.root == None:
+#             return False  
+#         temp = self.root 
+#         while temp is not None:
+#             if value < temp.value:
+#                 temp = temp.left 
+#             elif value > temp.value:
+#                 temp = temp.right
+#             else:
+#                 return True 
+#             return False  
+            
+# my = BinarySearchTree()
+# my.insert(4)
+# my.insert(5)
+# print(my.contains(1))
+
+
+
+    def insert(self, value):
         new_node = Node(value)
         if self.root == None:
             self.root = new_node
-            return True 
-        temp = self.root
+            return True
+        temp = self.root 
         while (True):
-            if new_node.value == temp.value:
-                return False
+            if new_node.value ==  temp.value:
+                return False 
             if new_node.value < temp.value:
                 if temp.left is None:
                     temp.left = new_node
-                    return True 
+                    return True
                 temp = temp.left 
             else:
                 if temp.right is None:
                     temp.right = new_node
                     return True 
+                temp= temp.right 
+
+
+    def contains(self,value):
+        temp = self.root
+        while temp is not None: 
+            if value < temp.value:
+                temp = temp.left 
+            elif value > temp.value:
                 temp = temp.right 
+            else:
+                return True
+        return False  
 
 
