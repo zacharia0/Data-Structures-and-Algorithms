48fd0ba1049db79dfadaf7c5488608f734463918
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-04 20:45:19 -0600

    Added the get_item() method which fetches an item from teh hash table.

--- hash_tables.py
+++ hash_tables.py
@@ -21,8 +21,20 @@ class HashTable:
         self.data_map[index].append([key, value])
 
 
-my = HashTable()
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+            
 
-my.set_item("bolts", 1400)
-my.set_item("washers", 50)
-my.print_table()
+
+
+my = HashTable()
+my.set_item('bolts', 1400)
+my.set_item('washers',50)
+my.get_item("bolts")
+print(my.get_item('bolts'))
+my.print_table()        
