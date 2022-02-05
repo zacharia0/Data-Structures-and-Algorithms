6b002f3b1fe599cc7a5b7c32a7c7352a1b720e71
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-04 20:33:36 -0600

    Fixed errors.

--- hash_tables.py
+++ hash_tables.py
@@ -1,19 +1,19 @@
 class HashTable:
-    def __init__(self, size = 7):
+    def __init__(self,size = 7):
         self.data_map = [None] * size 
-    
+
+
     def __hash(self, key):
-        my_hash = 0
-        for letter in key:
-            my_hash =(my_hash + ord(letter) * 23) % len(self.data_map)
+        my_hash = 0 
+        for letter in str(key):
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
 
-    
     def print_table(self):
         for i, val in enumerate(self.data_map):
-            print(f"{i} : {val}")
+            print(f"{i}: {val}")
 
-
+    
     def set_item(self, key, value):
         index = self.__hash(key)
         if self.data_map[index] == None:
@@ -21,4 +21,8 @@ class HashTable:
         self.data_map[index].append([key, value])
 
 
+my = HashTable()
 
+my.set_item("bolts", 1400)
+my.set_item("washers", 50)
+my.print_table()
