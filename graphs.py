7ce3559046037ba5f59ec115933bf569949cb055
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-07 22:51:56 -0600

    Added the add_edge() method which adds edge between two vertices.

--- graphs.py
+++ graphs.py
@@ -1,13 +1,29 @@
 class Graph:
     def __init__(self):
-        self.adjancency_list = {}
+        self.adjacency_list = {}
 
     def print_graph(self):
-        for vertex in self.adjancency_list:
-            print(f"{vertex} : {adjancency_list[vertex]}")
+        for vertex in self.adjacency_list:
+            print(f"{vertex} : {self.adjacency_list[vertex]}")
+
 
     def add_vertex(self, vertex):
-        if vertex is not in self.adjancency_list.keys():
-            self.adjancency_list[vertext] = []
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
             return True 
         return False 
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True 
+        return False
+
+my = Graph()
+
+my.add_vertex("A")
+my.add_vertex("B")
+
+my.add_edge("A","B")
+my.print_graph()
