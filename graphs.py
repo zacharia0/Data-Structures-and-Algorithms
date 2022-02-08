34c27f473d96361d1722786379101d4b39c9b0c1
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-07 22:29:22 -0600

    Added the print_graph() method and the add_vertex() method with adds a vertex into the graph.

--- graphs.py
+++ graphs.py
@@ -2,5 +2,12 @@ class Graph:
     def __init__(self):
         self.adjancency_list = {}
 
+    def print_graph(self):
+        for vertex in self.adjancency_list:
+            print(f"{vertex} : {adjancency_list[vertex]}")
 
-
+    def add_vertex(self, vertex):
+        if vertex is not in self.adjancency_list.keys():
+            self.adjancency_list[vertext] = []
+            return True 
+        return False 
