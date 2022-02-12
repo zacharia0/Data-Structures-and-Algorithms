3ef9008417b7a1be22fdca9edf0167cad71a0eed
Zacharia Dolal <zacharia.dolal@gmail.com>
2022-02-11 19:07:03 -0600

    Added the merge_sort() method which sorts a list.

--- merge_sort.py
+++ merge_sort.py
@@ -1,25 +1,37 @@
 def merge(list1, list2):
     combined = []
-    i = 0
-    j = 0 
+    i = 0 
+    j = 0
     while i < len(list1) and j < len(list2):
         if list1[i] < list2[j]:
             combined.append(list1[i])
             i += 1 
         else:
             combined.append(list2[j])
-            j += 1
+            j += 1 
 
     while i < len(list1):
         combined.append(list1[i])
         i += 1 
 
-    while i < len(list2):
-        combined.append(list[j])
-        j += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1 
     return combined
 
+def merge_sort(my_list):
+    if len(my_list) == 1:
+        return my_list 
+
+    mid = int(len(my_list) / 2)
+    left = my_list[:mid]
+    right = my_list[mid:]
+    return merge(merge_sort(left), merge_sort(right))
 
 
-print(merge([1,2,4], [7,8,9]))
-print(merge([1,2,7,8], [4,5,6]))
+print(merge_sort([9,4,3,3,2,1]))
+
+
+
+
+
